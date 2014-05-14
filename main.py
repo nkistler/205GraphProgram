bd4e89546e2fa7c26d3117f598e21b5d2c13c0ee
Nathan Kistler <nkistler@csumb.edu>
2014-05-14 11:40:06 -0700

    Added interval selection and error checking.

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ from datetime import timedelta
 homeDir = "/home/nathan/gmail-archive/"
 homeDirList = list() #contains our set of email to search
 listOfMatchingFiles = list() #contains emails that match our query
-listOfXValues = list() #contains list of three month intervals within date range
+listOfXValues = list() #assigns number for each quarter
+listOfXValuesAsDate = list() #contains list of three month intervals within date range
 listOfYValues = list() #contains frequency of term with each index containing the frequency of one quarter
-listOfDates = list() #contains list of dates, corrosponds to list of matching files
+listOfFileDates = list() #contains list of dates, corrosponds to list of matching files
 
 #initialize needed modules
 os.system("getmail -r /home/nathan/.getmail/getmailrc")
@@ -31,64 +32,50 @@ def getAllHomeFolder(x, dir_name, files):
             homeDirList.append(fullPath)
 os.path.walk(homeDir, getAllHomeFolder, 0)
 
-#Defines line graph
-class MyLine(lines.Line2D):
-   def __init__(self, *args, **kwargs):
-      # we'll update the position when the line data is set
-      self.text = mtext.Text(0, 0, '')
-      lines.Line2D.__init__(self, *args, **kwargs)
-
-      # we can't access the label attr until *after* the line is initialized
-      self.text.set_text(self.get_label())
-
-   def set_figure(self, figure):
-      self.text.set_figure(figure)
-      lines.Line2D.set_figure(self, figure)
-
-   def set_axes(self, axes):
-      self.text.set_axes(axes)
-      lines.Line2D.set_axes(self, axes)
-
-   def set_transform(self, transform):
-      # 2 pixel offset
-      texttrans = transform + mtransforms.Affine2D().translate(2, 2)
-      self.text.set_transform(texttrans)
-      lines.Line2D.set_transform(self, transform)
-
-
-   def set_data(self, x, y):
-      if len(x):
-         self.text.set_position((x[-1], y[-1]))
-
-      lines.Line2D.set_data(self, x, y)
-
-   def draw(self, renderer):
-      # draw my label at the end of the line with 2 pixel offset
-      lines.Line2D.draw(self, renderer)
-      self.text.draw(renderer)
-
 def graphBySearchTerm():
+    #initialize needed members
+    start = ""
+    end = ""
+    interval = ""
+    dateRE = re.compile(r'([0-9]*) ([A-z]{3}) ([0-9]{4})')
+    date = re.compile("Date: ")
+
     #get user input
     search = raw_input("Enter search term: ")
     search_term = re.compile(search)
-    start = raw_input("Enter start date: (Ex. 2 Feb 2012)")
+    while (not re.match(dateRE, start)):
+        start = raw_input("Enter start date: (Ex. 2 Feb 2012)")
     start_date = datetime.strptime(start, '%d %b %Y')
-    end = raw_input("Enter end date: ");
+    while (not re.match(dateRE, end)):
+        end = raw_input("Enter end date: ")
     end_date = datetime.strptime(end, '%d %b %Y')
-
-    #get list of x values (every 3 months within range of start and end dates)
-    three_months = timedelta(91, 26827, 2)
-    while (start_date < end_date):
-        listOfXValues.append(start_date);
-        start_date = start_date + three_months
-    listOfXValues.append(end_date)
+    while (not re.search(r'[MmQq]', interval)):
+        interval = raw_input("Graph by quarter (Q) or month (M)?")
+    
+    #set interval
+    if (re.search(r'[Qq]', interval)):
+        interval = "Q"
+        #get list of x values (every 3 months within range of start and end dates)
+        three_months = timedelta(91, 26827, 2)
+        while (start_date < end_date):
+            listOfXValuesAsDate.append(start_date)
+            start_date = start_date + three_months
+        listOfXValuesAsDate.append(end_date)
+    else:
+        interval = "M"
+        #get list of x values (every month within range of start and end dates)
+        one_month = timedelta(30, 37739, 520000)
+        while (start_date < end_date):
+            listOfXValuesAsDate.append(start_date)
+            start_date = start_date + one_month
+        listOfXValuesAsDate.append(end_date)
 
     #This searches our list of email files for the expression which the user entered
     for entry in homeDirList:
-        #variables needed to handle every file
-        date = re.compile("Date: ")
-        fileList = list()
+        #variables needed to handle every file 
         dateFound = False
+        date_object = None
+        this_file_array = list()
 
         #open file
         fileObject = open(entry, "r")
@@ -101,24 +88,27 @@ def graphBySearchTerm():
                 if (subline[6]==" "):#this handles case where day of the month is only one digit
                     subline = subline[0:24]
                 date_object = datetime.strptime(subline, '%a, %d %b %Y %H:%M:%S')
-                fileList.append(date_object)
                 dateFound = True
 
             #logs file name and date if the search term is found
             if (search_term.findall(line)):
-                listOfDates.append(fileList[-1])
-                fileList.append(entry)
-                listOfMatchingFiles.append(fileList)
+                listOfFileDates.append(date_object)
+                this_file_array.append(date_object)
+                this_file_array.append(entry)
+                listOfMatchingFiles.append(this_file_array)
                 break
+        fileObject.close()
     
-    #initialize y values to zero
-    for x in xrange(len(listOfXValues)):
-        listOfYValues.append(0);
+    #initialize plot values
+    for x in xrange(len(listOfXValuesAsDate)-1):
+        listOfYValues.append(0)
+        listOfXValues.append(x)
+   
     
     #Check dates of each file, check which date range they fit into, and add a +1 for frequency of term (our Y value)
-    for x in xrange(len(listOfDates)):
-        for y in xrange(len(listOfXValues)-1):
-            if listOfDates[x] >= listOfXValues[y] and listOfDates[x] < listOfXValues[y+1]:
+    for x in xrange(len(listOfFileDates)-1):
+        for y in xrange(len(listOfXValuesAsDate)-1):
+            if (listOfFileDates[x] >= listOfXValuesAsDate[y] and listOfFileDates[x] < listOfXValuesAsDate[y+1]):
                 listOfYValues[y] = listOfYValues[y]+1
                 
 
@@ -130,20 +120,32 @@ def graphBySearchTerm():
     for entry in listOfMatchingFiles:
         for item in entry:
             print item
+    
+    return interval
 
 #Contains main program loop. New modules can be added here.
 def main():
+    loop = True
+    while (loop == True):
+        timeInterval = graphBySearchTerm()
+    
+        plt.plot(listOfXValues, listOfYValues)
+    
+        plt.xlabel('Time')
+        plt.ylabel('Frequency')
 
-    graphBySearchTerm()
-    fig, ax = plt.subplots()
-    x, y = (listOfXValues, listOfYValues)#set of x should be our time value and set of y should be our frequency value
-    line = MyLine(x, y)
-    #line.text.set_text('line label')
+        if (timeInterval == "Q"):
+            plt.title('Search Term Frequency By Quarter')
+        else:
+            plt.title('Search Term Frequency By Month')
 
-    ax.set_title('Frequency of Search Term Over Time')
-    ax.add_line(line)
+        plt.show()
 
-    plt.show()
+        loopCmd = raw_input("Run again? (Y/N) ")
+        if (re.match(r'[Yy]', loopCmd)):
+            loop = True
+        else:
+            loop = False
 
 #Execute
 main()
